16d6bdfd854fa3f361437b64600b14c75757ac0a
Eric Higgins <ericdhiggins@gmail.com>
2021-04-28 17:40:17 -0500

    Get data from wireless receiver and imu

--- src/FinalProjectPlugin.cpp
+++ src/FinalProjectPlugin.cpp
@@ -2,13 +2,15 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
+#include <gazebo/sensors/sensors.hh>
+#include <gazebo/transport/Subscriber.hh>
 
 namespace gazebo
 {
     class FinalProjectPlugin : public ModelPlugin
     {
     public:
-        void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+        void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
         {
             // Store the pointer to the model
             this->model = _parent;
@@ -17,23 +19,55 @@ namespace gazebo
             // simulation iteration.
             this->updateConnection = event::Events::ConnectWorldUpdateBegin(
                 std::bind(&FinalProjectPlugin::OnUpdate, this));
+
+            std::string wirelessName = _sdf->Get("wirelessName", static_cast<std::string>("wireless_sensor")).first;
+            std::string imuName = _sdf->Get("imuName", static_cast<std::string>("imu_sensor")).first;
+
+            std::vector<std::string> wirelessScopedName = this->model->SensorScopedName(wirelessName);
+            std::vector<std::string>
+                imuScopedName = this->model->SensorScopedName(imuName);
+
+            if (wirelessScopedName.size() > 1)
+            {
+                gzwarn << "Multiple names match " << wirelessName << ". Failing.\n";
+            }
+            else
+            {
+                this->wireless = std::dynamic_pointer_cast<sensors::WirelessReceiver>(sensors::SensorManager::Instance()->GetSensor(wirelessScopedName[0]));
+                transport::NodePtr node(new transport::Node());
+                node->Init();
+
+                this->subscriber = node->Subscribe(this->wireless->Topic(), &FinalProjectPlugin::ReceiveWirelessData, this);
+                this->wirelessNode = node;
+            }
+
+            if (imuScopedName.size() > 1)
+            {
+                gzwarn << "Multiple names match " << imuName << ". Failing.\n";
+            }
+            else
+            {
+                this->imu = std::dynamic_pointer_cast<sensors::ImuSensor>(sensors::SensorManager::Instance()->GetSensor(imuScopedName[0]));
+            }
         }
 
-        // Called by the world update start event
-    public:
         void OnUpdate()
         {
-            // Apply a small linear velocity to the model.
-            this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+        }
+
+        void ReceiveWirelessData(ConstWirelessNodesPtr &_msg)
+        {
+            gzmsg << _msg->node_size() << "\n";
         }
 
         // Pointer to the model
     private:
         physics::ModelPtr model;
-
-        // Pointer to the update event connection
-    private:
         event::ConnectionPtr updateConnection;
+        sensors::ImuSensorPtr imu;
+        sensors::WirelessReceiverPtr wireless;
+        transport::NodePtr wirelessNode;
+        transport::SubscriberPtr subscriber;
     };
 
     // Register this plugin with the simulator
